cedcb452589c45617d9ed318022ba90108f92eea
Federico Margueliche <Fede_FJM@hotmail.com>
2018-11-21 08:29:10 -0300

    Correccion de errores y mejoras.

--- telegram-bot/controller/BotController.php
+++ telegram-bot/controller/BotController.php
@@ -3,83 +3,53 @@
     class BotController{
         public static function startBot(){
             $returnArray = true;
-            $rawData = file_get_contents('php://input');
-            $response = json_decode($rawData, $returnArray);
-            $id_del_chat = $response['message']['chat']['id'];
-             
-            // Obtener comando (y sus posibles parametros)
-            $regExp = '#^(\/[a-zA-Z0-9\/]+?)(\ .*?)$#i';
+            $update = json_decode(file_get_contents('php://input') , $returnArray);
+            $cmd = $update['message']['text'];
+            $chat_id = $update['message']['chat']['id'];
+            $name = $update['message']['from']['first_name'];
 
-
-            $tmp = preg_match($regExp, $response['message']['text'], $aResults);
-
-            if (isset($aResults[1])) {
-                $cmd = trim($aResults[1]);
-                $cmd_params = trim($aResults[2]);
-            } else {
-                $cmd = trim($response['message']['text']);
-                $cmd_params = '';
-            }
-             
-            // Mensaje de respuesta
-            $msg = array();
-            $msg['chat_id'] = $response['message']['chat']['id'];
-            $msg['text'] = null;
-            $msg['disable_web_page_preview'] = true;
-            $msg['reply_to_message_id'] = (int)$response['message']['message_id'];
-            $msg['reply_markup'] = null;
-            
             $comando=explode(':',$cmd);
 
             if (sizeof($comando)>1){
                 $cmd=$comando[0];
             }
 
-            // Comandos
+            // Comandos que responde el bot
+
+            $msg='';
+
             switch ($cmd) {
                 case '/start':
-                    $msg['text']  = 'Hola ' . $response['message']['from']['first_name'] . " Usuario: " . $response['message']['from']['username'] . '!' . PHP_EOL;
-                    $msg['text'] .= '¿Como puedo ayudarte? Puedes ver una lista de las opciones disponibles con el comando /help';
-                    $msg['reply_to_message_id'] = null;
+                    $msg = 'Hola '.$name.'¿Que necesita? Si escribis /help te listare todos los comandos disponibles.';
                     break;
              
                 case '/help':
-                    $msg['text']  = 'Los comandos disponibles son estos:' . PHP_EOL;
-                    $msg['text'] .= '/start Inicializa el bot' . PHP_EOL;
-                    $msg['text'] .= '/instituciones Muestra las instituciones disponibles.' . PHP_EOL;
-                    $msg['text'] .= '/instituciones-region-sanitaria: {region-sanitaria​​}: Muestra un listado de Instituciones a partir de una la región sanitaria indicada.' . PHP_EOL;
-                    $msg['text'] .= '/help Muestra los comandos disponibles (Los que te estoy mostrando ahora).';
-                    $msg['reply_to_message_id'] = null;
+                    $msg = 'Los comandos disponibles son los siguientes: /start Inicializa el bot. /instituciones Muestra las instituciones disponibles. /instituciones-region-sanitaria: {region-sanitaria​​} Muestra un listado de Instituciones a partir de una la región sanitaria indicada. /help Muestra los comandos disponibles (Los que te estoy mostrando ahora). Espero tu comando!!!';
                     break;
              
                 case '/instituciones':
-                    $msg['text']  = "Las instituciones disponibles son: \n";
+                    $msg = "Las instituciones disponibles son:";
                     $instituciones = InstitucionesRepository::getInstitucion();
                     foreach ($instituciones as $institucion){
-                        $msg['text'] .= $institucion['nombre']."\n ";
+                        $msg = $msg.$institucion['nombre']." - ";
                     }
                     break;
 
-                case '/instituciones-region-sanitaria:':
-                    $msg['text']  = "Las instituciones disponibles para la region sanitaria indicada son: \n";
+                case '/instituciones-region-sanitaria':
+                    $msg = "Las instituciones disponibles para la region sanitaria ".$comando[1]." son: ";
                     $instituciones = InstitucionesRepository::getInstitucionRegionId($comando[1]);
                     foreach ($instituciones as $institucion){
-                        $msg['text'] .= $institucion['nombre']."\n ";
+                        $msg = $msg.$institucion['nombre']." - ";
                     }
-                    break;
-             
-                case '/info':
-                    $msg['text']  = json_encode($response);
                     break;
              
                 default:
-                    $msg['text']  = 'Lo siento ' . $response['message']['from']['first_name'] . ', pero [' . $cmd . '] no es un comando válido.' . PHP_EOL;
-                    $msg['text'] .= 'Prueba /help para ver la lista de comandos disponibles';
+                    $msg  = 'Lo siento '.$update['message']['from']['first_name'].', pero ['.$cmd.'] no puedo ayudarte con lo que me solicitas. Escribi el comando /help para ver la lista de comandos disponibles (los que entiendo).';
                     break;
             }
 
-            //Realizamos el envío
-            $url = 'https://api.telegram.org/bot794469660:AAFzyw5Ue3NfYqwtE15_H5F0ba2NDPyoKs0/sendMessage?text='.$msg['text'].'&chat_id='.$msg['chat_id'];
+            //Envio de respuesta
+            $url = 'https://api.telegram.org/bot794469660:AAFzyw5Ue3NfYqwtE15_H5F0ba2NDPyoKs0/sendMessage?text='.$msg.'&chat_id='.$chat_id;
             file_get_contents($url);
         }
     }
--- telegram-bot/index.php
+++ telegram-bot/index.php
@@ -1,54 +1,8 @@
 <?php
 
 include_once 'model/InstitucionesRepository.php';
+include_once 'controller/BotController.php';
 
-$returnArray = true;
-$update = json_decode(file_get_contents('php://input') , $returnArray);
-$cmd = $update['message']['text'];
-$chat_id = $update['message']['chat']['id'];
-$name = $update['message']['from']['first_name'];
+BotController::startBot();
 
-$comando=explode(':',$cmd);
-
-if (sizeof($comando)>1){
-    $cmd=$comando[0];
-}
-
-// Comandos
-
-$msg='';
-
-switch ($cmd) {
-    case '/start':
-        $msg = 'Hola '.$name.'¿Como puedo ayudarte? Puedes ver una lista de las opciones disponibles con el comando /help';
-        break;
- 
-    case '/help':
-        $msg = 'Los comandos disponibles son estos: /start Inicializa el bot. /instituciones Muestra las instituciones disponibles. /instituciones-region-sanitaria: {region-sanitaria​​} Muestra un listado de Instituciones a partir de una la región sanitaria indicada. /help Muestra los comandos disponibles (Los que te estoy mostrando ahora).';
-        break;
- 
-    case '/instituciones':
-        $msg = "Las instituciones disponibles son:";
-        $instituciones = InstitucionesRepository::getInstitucion();
-        foreach ($instituciones as $institucion){
-            $msg = $msg.$institucion['nombre']." - ";
-        }
-        break;
-
-    case '/instituciones-region-sanitaria':
-        $msg = "Las instituciones disponibles para la region sanitaria ".$comando[1]." son: ";
-        $instituciones = InstitucionesRepository::getInstitucionRegionId($comando[1]);
-        foreach ($instituciones as $institucion){
-            $msg = $msg.$institucion['nombre']." - ";
-        }
-        break;
- 
-    default:
-        $msg  = 'Lo siento '.$update['message']['from']['first_name'].', pero ['.$cmd.'] no es un comando válido. Prueba /help para ver la lista de comandos disponibles';
-        break;
-}
-
-//Realizamos el envío
-$url = 'https://api.telegram.org/bot794469660:AAFzyw5Ue3NfYqwtE15_H5F0ba2NDPyoKs0/sendMessage?text='.$msg.'&chat_id='.$chat_id;
-file_get_contents($url);
 ?>
