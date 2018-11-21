b7be26c8f82f34bbbbffe9dce80aea9de0ed6699
Federico Margueliche <Fede_FJM@hotmail.com>
2018-11-21 08:18:56 -0300

    Correccion de errores.

--- telegram-bot/index.php
+++ telegram-bot/index.php
@@ -37,9 +37,9 @@ switch ($cmd) {
 
     case '/instituciones-region-sanitaria':
         $msg = "Las instituciones disponibles para la region sanitaria ".$comando[1]." son: ";
-        $insts = InstitucionesRepository::getInstitucionRegionId($comando[1]);
-        foreach ($insts as $i){
-            $msg = ":heavy_minus_sign:".$msg.$i['nombre'];
+        $instituciones = InstitucionesRepository::getInstitucionRegionId($comando[1]);
+        foreach ($instituciones as $institucion){
+            $msg = $msg.$institucion['nombre']." - ";
         }
         break;
  
--- telegram-bot/model/InstitucionesRepository.php
+++ telegram-bot/model/InstitucionesRepository.php
@@ -4,7 +4,8 @@
         /* Todas las funciones retornan un arreglo, el cual contiene ademas un arreglo por cada elemento (como si fuera un FetchArray de una consulta sql); en excepcion de las consultas que requieren un ID que traen solo un arreglo con la informacion del elemento del ID correspondiente. */
 
         private static function getData($data){
-            return json_decode(file_get_contents("https://grupo11.proyecto2018.linti.unlp.edu.ar/api-instituciones/".$data),true);
+            $url = filter_var("https://grupo11.proyecto2018.linti.unlp.edu.ar/api-instituciones/".$data,FILTER_SANITIZE_URL);
+            return json_decode(file_get_contents($url),true);
         }
 
         public static function getInstitucion() {
