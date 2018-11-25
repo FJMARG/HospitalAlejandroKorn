cd3813aaf746a096b94a1495893d423883505a45
MARRERO, FACUNDO <facu_marrero@hotmail.com>
2018-11-25 15:54:00 -0300

    vista triple en reportes

---
+++ highcharts/estadisticoXLocalidad.js
@@ -0,0 +1,43 @@
+Highcharts.chart('localidad', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Estadistica por Localidad'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'chascomus',
+            y: 10.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Necochea',
+            y: 11.84
+        }, {
+            name: 'La Plata',
+            y: 2.61
+        }]
+    }]
+});
