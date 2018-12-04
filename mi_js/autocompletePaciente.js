2eed580656512d2cbc56dd0635000ff503189d13
Pablo <pablitorusso25@hotmail.com>
2018-12-04 04:22:37 -0300

    cambios

--- mi_js/autocompletePaciente.js
+++ mi_js/autocompletePaciente.js
@@ -3,6 +3,14 @@ $(function() {
     // toquen de acceso 
     mapboxgl.accessToken = 'pk.eyJ1IjoicHJ1c3NvMjUiLCJhIjoiY2pvbjgzazJ2MHM3dTNxbzV6MHowYXZ1bCJ9.1rAOSL_0iJrGtCl4rq6tHw';
 
+
+    var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-58.3772300, -34.6083], // starting position
+    zoom: 8 // starting zoom
+    });
+
     // Add zoom and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl());
 
